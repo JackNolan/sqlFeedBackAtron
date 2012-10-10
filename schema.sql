ab8abc459404e50de56c07f1048e3c21c7a9e3d6
jacknolan <jack@jacknolan.com>
2012-10-10 14:46:31 -0400

    inital schema changes

---
+++ schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users 
+{
+	id INTEGER PRIMARY KEY,
+	user_type INTEGER NOT NULL,
+	name VARCHAR(255)
+};
+
+CREATE TABLE user_types 
+{
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(255)
+};
+
+CREATE TABLE issues 
+{
+	id INTEGER PRIMARY KEY,
+	author_id INTEGER NOT NULL,
+	status_id INTEGER NOT NULL,
+	created_at INTEGER NOT NULL,
+	content VARCHAR(255)
+};
+
+CREATE TABLE solutions 
+{
+	id INTEGER PRIMARY KEY,
+	user_type INTEGER NOT NULL,
+	name VARCHAR(255)
+};
