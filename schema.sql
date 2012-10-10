8b17e7621e6af8d82e33b3bf12ee6056853c3d3b
jacknolan <jack@jacknolan.com>
2012-10-10 15:20:32 -0400

    inital db pop

--- schema.sql
+++ schema.sql
@@ -1,38 +1,38 @@
 CREATE TABLE users 
-{
+(
 	id INTEGER PRIMARY KEY,
 	user_type INTEGER NOT NULL,
 	name VARCHAR(255)
-};
+);
 
 CREATE TABLE user_types 
-{
+(
 	id INTEGER PRIMARY KEY,
 	name VARCHAR(255)
-};
+);
 
 CREATE TABLE issues 
-{
+(
 	id INTEGER PRIMARY KEY,
 	author_id INTEGER NOT NULL,
 	status_id INTEGER NOT NULL,
 	created_at DATETIME NOT NULL,
 	content VARCHAR(255)
-};
+);
 
 CREATE TABLE statuses 
-{
+(
 	id INTEGER PRIMARY KEY,
 	name VARCHAR(255)
-};
+);
 
 
 CREATE TABLE solutions 
-{
+(
 	id INTEGER PRIMARY KEY,
 	author_id INTEGER NOT NULL,
 	issue_id INTEGER NOT NULL,
 	accepted BOOLEAN,
 	content VARCHAR(255)
-};
+);
 
