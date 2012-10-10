47387c6d6e3b0d490cdc20eea36f6eaaca45651d
jacknolan <jack@jacknolan.com>
2012-10-10 15:33:55 -0400

    fixed schema and data

--- data.sql
+++ data.sql
@@ -1,25 +1,25 @@
-INSERT INTO users (id, name, type)
-	VALUES (1, "Adam", 1), 
-		   (2, "Jack", 1),
-		   (3, "Avi", 2);
+INSERT INTO users (name, user_type)
+	VALUES ("Adam", 1), 
+		   ("Jack", 1),
+		   ("Avi", 2);
 
 
-INSERT INTO user_types (id, name)
-	VALUES (1, "Student"),
-	       (2, "Instructor");
+INSERT INTO user_types (name)
+	VALUES ("Student"),
+	       ("Instructor");
 
-INSERT INTO statuses (id, name)
-	VALUES (1, "New"), 
-		   (2, "Open"), 
-		   (3, "Instructor Needed"), 
-		   (4, "Self Solved"), 
-		   (5, "User Solved"), 
-		   (6, "Instructor Solved");
+INSERT INTO statuses (name)
+	VALUES ("New"), 
+		   ("Open"), 
+		   ("Instructor Needed"), 
+		   ("Self Solved"), 
+		   ("User Solved"), 
+		   ("Instructor Solved");
 
 
-INSERT INTO issues (id, author_id, status_id, created_at, content)
-	VALUES (1, 1, 1, 2012-10-10 15:05:22, "How do you get the timestamp with SQL?");
+INSERT INTO issues (author_id, status_id, created_at, content)
+	VALUES (1, 1, datetime('now'), "How do you get the timestamp with SQL?");
 
-INSERT INTO solutions (id, author_id, issue_id, accepted, content)
-	VALUES (1, 2, 1, 0, "use timestamp")
-		   (1, 3, 1, 1, "use DateTime")		
+INSERT INTO solutions (author_id, issue_id, accepted, content)
+	VALUES (2, 1, 0, "use timestamp"),
+		   (3, 1, 1, "use DateTime");	
